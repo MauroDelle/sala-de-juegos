9cceb9098a38a2bbfc127809c9b524e643e4d889
Mauro <maurodelle7@gmail.com>
2024-05-07 13:06:43 -0300

    Arreglando Home

--- src/app/modules/home/home/home.component.ts
+++ src/app/modules/home/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgComponentOutlet, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  gameNames: string[] = ["Buscaminas", "Nombre del Juego 2", "Nombre del Juego 3", "Nombre del Juego 4"];
+
+
+  highlightedGameIndex: number | null = null;
+
+  highlightGame(index: number) : void {
+
+    this.highlightedGameIndex = index;
+  }
+
+  removeHighlight() : void {
+    this.highlightedGameIndex = null;
+  }
+
 
 }
