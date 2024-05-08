8dbe92db46f068e432350799b16d1bb1fc693ad9
Mauro <maurodelle7@gmail.com>
2024-05-08 14:23:09 -0300

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"sala-juegos-e46aa","appId":"1:160629021929:web:774ddf7c34e4e4bf592478","storageBucket":"sala-juegos-e46aa.appspot.com","apiKey":"AIzaSyAv4G05PiQubu0wGhHdHw5scABK0FAVVPU","authDomain":"sala-juegos-e46aa.firebaseapp.com","messagingSenderId":"160629021929"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"sala-juegos-e46aa","appId":"1:160629021929:web:d78d005d1a9375ed592478","storageBucket":"sala-juegos-e46aa.appspot.com","apiKey":"AIzaSyAv4G05PiQubu0wGhHdHw5scABK0FAVVPU","authDomain":"sala-juegos-e46aa.firebaseapp.com","messagingSenderId":"160629021929"}))), importProvidersFrom(provideAuth(() => getAuth()))]
 };
--- src/app/modules/auth/components/register/register.component.ts
+++ src/app/modules/auth/components/register/register.component.ts
@@ -1,45 +1,15 @@
-import { CommonModule } from '@angular/common';
-import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent{
 
-  username: string = '';
-  email: string = '';
-  password: string = '';
-  confirmPassword: string = '';
 
-  constructor() {}
-
-  onSubmit(): void {
-    // Validar que las contraseñas coincidan
-    if (this.password !== this.confirmPassword) {
-      alert('Passwords do not match');
-      return;
-    }
-
-    // Guardar el usuario en el localStorage (o en tu backend)
-    const newUser = {
-      username: this.username,
-      email: this.email,
-      password: this.password
-    };
-
-    // Aquí puedes enviar los datos a tu backend para guardarlo en la base de datos
-
-    // Ejemplo de almacenamiento en localStorage
-    localStorage.setItem('user', JSON.stringify(newUser));
-
-    alert('Registration successful!');
-    // Redirigir a la página de inicio de sesión o realizar cualquier otra acción necesaria
-  }
 
 }
