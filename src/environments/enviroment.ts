bf12f474cbf2950c9dcf8bf6fe44df17c1e4b6c9
Mauro <maurodelle7@gmail.com>
2024-05-09 10:41:36 -0300

    environments

---
+++ src/environments/enviroment.ts
@@ -0,0 +1,8 @@
+
+
+
+export const Enviroment = {projectId:"sala-juegos-e46aa",appId:"1:160629021929:web:d78d005d1a9375ed592478",
+storageBucket:"sala-juegos-e46aa.appspot.com",
+apiKey:"AIzaSyAv4G05PiQubu0wGhHdHw5scABK0FAVVPU",
+authDomain:"sala-juegos-e46aa.firebaseapp.com",
+messagingSenderId:"160629021929"}
